dd330a1998a6b6d0567b06a6180a13870590c11b
skyeha <nhatanhhado@gmail.com>
2024-05-02 14:47:26 +1000

    asd

--- ui.R
+++ ui.R
@@ -39,6 +39,12 @@ ui <- dashboardPage(
         fluidRow(tags$div(tags$strong("Welcome!"), style = "font-size:22px")),
         fluidRow(HTML(r"(
           <p style="font-size:16px">This is a Clinical Dashboard that offers multiple interactive visualisations </p>
+          <p style="font-size:16px;">By inputing a standard RedCap project's API, this dashboard automatically generate mutiple visualization to provide user a better understanding of the studied disease.</p>
+          <p style="font-size:16px;">Currently, it allows user to view disease distribution by suburb through a interactive map that supports Melbourne and entire Victora region. 
+          It also provides a Kaplan-Meier Plot that compare the effects of most commonly used two medicine on patient's death rate.</p>
+          <p style="font-size:16px;">You can preview its effect by selecting three defaultly provided database. You can find the original RedCap through link below.
+          Notice that all of these database use stimulated data generated from <a href="https://github.com/synthetichealth/synthea" target="_blank">Synthea<sup>TM</sup></a>, a synthetic patient generator that models the medical history of synthetic patients, for testing purpose.</p>
+          <br></br>
         )")),
         # Example: Create a box with some content
         box(
