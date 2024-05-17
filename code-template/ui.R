60aff19ef2c3622f0dc2432ccc18ecee4c5e0f5f
skyeha <nhatanhhado@gmail.com>
2024-05-17 16:25:48 +1000

    asd

--- code-template/ui.R
+++ code-template/ui.R
@@ -2,12 +2,6 @@ library(shiny)
 
 
 ui <- fluidPage(
-    fluidRow(
-          actionButton(inputId='toRegistry',
-            label="Back to registry",
-            icon = icon("th"),
-            onclick= "window.history.back()")
-        ),
     includeMarkdown("test.Rmd")
 )
 
